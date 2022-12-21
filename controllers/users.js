37341082034f91c9a0b33b54a7dc1a648161ad78
dudahamilton <mariaehamilton28@gmail.com>
2022-12-21 13:42:16 -0800

    finishes part 2 login and logout

--- controllers/users.js
+++ controllers/users.js
@@ -32,5 +32,51 @@ router.post('/', async (req, res) =>{
 
     }
 })
+
+// GET /users.login -- render a login form that POSTs to /users/login
+router.get('/login', (req, res) => {
+    res.render('users/login.ejs', {
+        message: req.query.message ? req.query.message : null
+    })
+})
+//POST /users/login -- ingest data from rendered @ GET /users/login
+router.post('/login', async (req, res)=>{
+    try {
+        //look up the user based on their email
+        const user = await db.user.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+        //boilerplate message if loging dails
+        const badCredentialMessage = 'username or password incorrect'
+        if (!user) {
+            // if the user isnt found in the db
+            res.redirect('/users/login?message=' + badCredentialMessage)
+        } else if (user.password !== req.body.password) {
+           // if the users supplied password is incorrect
+           res.redirect('/users/login?message=' +badCredentialMessage)
+        }else {
+             //if the user 
+             console.log('loggin user in!')
+             res.cookie('userId', user.id)
+             res.redirect('/')
+        }
+
+    } catch (err) {
+        console.log(err)
+        res.status(500).send('server error')
+    }
+})
+
+// GET /users/logout --clear any cookies and redirect to homepage
+router.get('/logout', (req, res) => {
+    // og the user out by removing the cookie
+    // make a get req to /
+    res.clearCookie('userId')
+    res.redirect('/')
+})
+
+
 //export the router
 module.exports = router
